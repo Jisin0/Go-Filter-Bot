572db00284fa3de9e9bff212261eb0c39208242a
Jisin Jose <79639422+Jisin0@users.noreply.github.com>
2024-05-16 19:32:24 +0100

    Bump Versions
    Update to latest gotgbot version based on bot API 7.3
    Adds a new fresh cleaner conversation implementation

--- main.go
+++ main.go
@@ -37,8 +37,13 @@ func main() {
 
 	// Create bot from environment value.
 	b, err := gotgbot.NewBot(token, &gotgbot.BotOpts{
-		Client: http.Client{},
-		APIURL: gotgbot.DefaultAPIURL,
+		BotClient: &gotgbot.BaseBotClient{
+			Client: http.Client{},
+			DefaultRequestOpts: &gotgbot.RequestOpts{
+				Timeout: gotgbot.DefaultTimeout, // Customise the default request timeout here
+				APIURL:  gotgbot.DefaultAPIURL,  // As well as the Default API URL here (in case of using local bot API servers)
+			},
+		},
 	})
 	if err != nil {
 		panic("failed to create new bot: " + err.Error())
@@ -52,25 +57,20 @@ func main() {
 	}
 
 	// Create updater and dispatcher.
-	updater := ext.NewUpdater(&ext.UpdaterOpts{
-		ErrorLog: nil,
-		DispatcherOpts: ext.DispatcherOpts{
-			// If an error is returned by a handler, log it and continue going.
-			Error: func(_ *gotgbot.Bot, _ *ext.Context, err error) ext.DispatcherAction {
-				fmt.Println("an error occurred while handling update:", err.Error())
-				return ext.DispatcherActionNoop
-			},
-			MaxRoutines: ext.DefaultMaxRoutines,
+	dispatcher := ext.NewDispatcher(&ext.DispatcherOpts{
+		// If an error is returned by a handler, log it and continue going.
+		Error: func(b *gotgbot.Bot, ctx *ext.Context, err error) ext.DispatcherAction {
+			fmt.Println("an error occurred while handling update:", err.Error())
+			return ext.DispatcherActionNoop
 		},
+		MaxRoutines: ext.DefaultMaxRoutines,
 	})
-	dispatcher := updater.Dispatcher
+	updater := ext.NewUpdater(dispatcher, &ext.UpdaterOpts{})
 
 	//Add update handlers
 	dispatcher.AddHandlerToGroup(handlers.NewCommand("start", plugins.Start), 1)
-	dispatcher.AddHandler(handlers.NewCallback(callbackquery.Equal("stats"), plugins.CbStats))
+	dispatcher.AddHandlerToGroup(handlers.NewCallback(callbackquery.Equal("stats"), plugins.CbStats), 3)
 	dispatcher.AddHandlerToGroup(handlers.NewCommand("stats", plugins.Stats), 3)
-	dispatcher.AddHandlerToGroup(handlers.NewMessage(message.All, utils.IsListening), 5)
-	//	dispatcher.AddHandlerToGroup(handlers.NewMessage(message.All, plugins.FilterHandler), 4)
 	dispatcher.AddHandlerToGroup(handlers.NewCommand("filter", plugins.NewFilter), 1)
 	dispatcher.AddHandlerToGroup(handlers.NewCommand("gfilter", plugins.NewFilter), 1)
 	dispatcher.AddHandlerToGroup(handlers.NewCommand("filters", plugins.AllMfilters), 2)
@@ -88,6 +88,8 @@ func main() {
 	dispatcher.AddHandlerToGroup(handlers.NewCallback(callbackquery.Prefix("stopf("), plugins.CbStop), 3)
 	dispatcher.AddHandlerToGroup(handlers.NewCallback(callbackquery.Prefix("edit("), plugins.CbEdit), 3)
 	dispatcher.AddHandlerToGroup(handlers.NewMessage(customfilters.PrivateOrGroup, plugins.FilterHandler), 1)
+
+	dispatcher.AddHandlerToGroup(handlers.NewMessage(message.All, utils.RunListening), 5)
 
 	// Start receiving updates.
 	err = updater.StartPolling(b, &ext.PollingOpts{DropPendingUpdates: true})
